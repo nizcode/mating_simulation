cf9ecf075a467a6de21041480679c5a55e778484
Neil Farrugia <nizzle10@github.com>
2019-08-09 17:50:41 +0100

    latest updates

--- oddsratio.R
+++ oddsratio.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 #---Odds ratio ---
 
 # Naisha was calculating was ((mum with gene)/(mum no gene))/((dad with gene)/(dad no gene)) = 1.63
@@ -5,7 +6,19 @@
 # if genotype is 1(Aa) then the score is 0.5 for that person
 # if genotype is 2(aa) then the score is 1 for that person
 
-data <- get(load(paste0('gsp_sim1000_100_',d,'.Rdata')))
+# type in which disassortive you want to look at by writing in argument 1 the degree
+args <- commandArgs(trailingOnly = TRUE)
+numargs <- length(args)
+enumargs <- 4 
+if(numargs != enumargs) {
+  print("sample population number, the number of simuations, amount of disassortive mating you want in decimal, and the number of couples you want in parent generation")
+  stop("Stopping right here")
+}
+N <- args[1]
+n <- args[2]
+d <- args[3]
+psize <- args[4]
+data <- get(load(paste0('gsp_sim',N,'_',n,'_',d,'_',psize,'_','.Rdata')))
 
 carr <- function(gty){
   ca1<-gty[2] * 0.5
@@ -30,3 +43,6 @@ for(i in 1:5){
 
 g2c<-cbind( matrix(data[[4]][,7], ncol = 1),  matrix(data[[3]][,7], ncol = 1) )
 oddr <- apply(g2c,1,function(x) x[1]/x[2])
+
+write.table(oddr, file = paste0('oddsRatio_Parents_',N,'_',n,'_','_',d,'_',psize,'_','.txt'))
+cat(paste('this is the average of the odds ratio for the parents',mean(oddr),'\n', sep = ' '))
